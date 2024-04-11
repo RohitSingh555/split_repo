6904fe7835c5b194b94f0a84c445c261f78d3028
RohitSingh555 <singhrg@rknec.edu>
2024-04-11 14:59:12 +0530

    small fixes related to vercel

--- split-stay-app/src/components/Navbar.jsx
+++ split-stay-app/src/components/Navbar.jsx
@@ -11,8 +11,8 @@ const Navbar = () => {
   return (
     <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 py-3">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="../../public/SplitStay.png" className="h-10" alt="SplitStay Logo" />
+        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src="/SplitStay.png" className="h-10" alt="SplitStay Logo" />
         </a>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <div className="md:flex gap-5 hidden">

0e470a988ccd9cd24537cc43eb9b6fc0fb856d95
RohitSingh555 <singhrg@rknec.edu>
2024-04-11 18:59:10 +0530

    small fix

--- split-stay-app/src/main.jsx
+++ split-stay-app/src/main.jsx
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
         errorElement: <ErrorPage />,
       },
       {
-        path: "about",
+        path: "/about",
             element: <AboutUs />,
             errorElement: <ErrorPage />,
       }

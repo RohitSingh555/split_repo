5bdff47149e09a76c973678553c85f2930b43714
RohitSingh555 <singhrg@rknec.edu>
2024-04-24 16:48:12 +0530

    added vercel.json and fixed about page

--- split-stay-app/src/components/Navbar.jsx
+++ split-stay-app/src/components/Navbar.jsx
@@ -11,6 +11,7 @@ const Navbar = () => {
 
   const location = useLocation();
   const isHome = location.pathname === "/";
+  const isAbout = location.pathname === "/about";
 
   return (
     <nav className="bg-white fixed w-full z-20 top-0 start-0 py-3 shadow-md">
@@ -22,7 +23,7 @@ const Navbar = () => {
           <img src="/SplitStay.png" className="h-10" alt="SplitStay Logo" />
         </Link>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          {isHome ? (
+          {isHome || isAbout ? (
             <div className="md:flex gap-5 hidden">
               <Link to={"/login"}>
                 <HomeButtons_Hollow
@@ -89,7 +90,7 @@ const Navbar = () => {
           }`}
           id="navbar-sticky"
         >
-          {isHome ? (
+          {isHome || isAbout ? (
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white ">
               <li>
                 <Link
